ff7afb6452592986df5d70ea0bfc1c44c4583d20
Irfan Nazir <179750083+irrfannazir@users.noreply.github.com>
2026-03-23 19:22:11 +0530

    analyze the expression

--- alt-parse/src/analyze.c
+++ alt-parse/src/analyze.c
@@ -24,49 +24,79 @@ int count_tree_needed_words(char *line){
     return count;
 }
 
-int check_line(struct Node *ptr, char *syn_line){
+int handle_tertiary_op(struct Node *ptr, char *word, int ci) {
+    if (ptr->right != NULL) {
+        ptr->right->right = createNode(strdup(word), EXPRESSION, ptr->start, ci - ptr->start);
+        ptr->right->left = createNode(NULL, EXPRESSION, ci + 1, -1);
+    } else {
+        ptr->left = createNode(strdup(word), EXPRESSION, ptr->start, ci - ptr->start);
+        ptr->right = createNode(NULL, BINARY_EXPRESSION, -1, -1);
+        ci--;
+    }
+    return ci;
+}
+
+void handle_binary_op(struct Node *ptr, char *word, int ci) {
+    ptr->left = createNode(NULL, OPERATOR, ci, 1);
+    ptr->right = createNode(NULL, BINARY_EXPRESSION, -1, -1);
+    ptr->right->right = createNode(strdup(word), EXPRESSION, ptr->start, ci - ptr->start);
+    ptr->right->left = createNode(NULL, EXPRESSION, ci + 1,
+        (ptr->start + ptr->size) - (ci + 1));
+}
+
+void assign_last_word(struct Node *ptr, char *word, int nos) {
+    if (ptr->right && ptr->right->right) {
+        if (nos == 3) {
+            ptr->right->left->format = strdup(word);
+            ptr->right->left->size =
+                (ptr->start + ptr->size) - ptr->right->left->start;
+        } else if (nos == 2) {
+            ptr->right->left->format = strdup(word);
+            ptr->right->left->size =
+                (ptr->start + ptr->size) - ptr->right->left->start;
+        }
+    }
+}
+
+int find_matching_token(int ci, struct Node *ptr, char *end) {
+    while (!compare_the_word(end, get_token(ci)) &&
+           ci < ptr->start + ptr->size) {
+        ci++;
+    }
+    return ci;
+}
+
+int check_line(struct Node *ptr, char *syn_line) {
     int tn = 0;
     int nos = count_tree_needed_words(syn_line);
     int ci = ptr->start;
+
     char *word = tokenize_white_space(syn_line, tn++);
-    while(word != NULL){
-        if(does_tree_needed(word)){
+
+    while (word != NULL) {
+
+        if (does_tree_needed(word)) {
+
             char *end = tokenize_white_space(syn_line, tn++);
-            if(end != NULL){
-                while(!compare_the_word(end, get_token(ci)) && ci < ptr->start + ptr->size){
-                    ci++;
-                }
-                if(ci == ptr->start + ptr->size){
+
+            if (end != NULL) {
+
+                ci = find_matching_token(ci, ptr, end);
+
+                if (ci == ptr->start + ptr->size) {
                     return 1;
                 }
-                if(nos == 3){
-                    if(ptr -> right != NULL){
-                        ptr -> right -> right = createNode(strdup(word), EXPRESSION, ptr->start, ci - ptr->start);
-                        ptr -> right -> left = createNode(NULL, EXPRESSION, ci + 1, -1);
-                    }else{
-                        ptr -> left = createNode(strdup(word), EXPRESSION, ptr -> start, ci - ptr -> start);
-                        ptr -> right = createNode(NULL, BINARY_EXPRESSION, -1, -1);
-                        ci--;
-                    }
+
+                if (nos == 3) {
+                    ci = handle_tertiary_op(ptr, word, ci);
+                } else if (nos == 2) {
+                    handle_binary_op(ptr, word, ci);
                 }
-                else if(nos == 2){
-                    ptr->left = createNode(NULL, OPERATOR, ci, 1);
-                    ptr->right = createNode(NULL, BINARY_EXPRESSION, -1, -1);
-                    ptr->right->right = createNode(strdup(word), EXPRESSION, ptr->start, ci - ptr->start);
-                    ptr->right->left = createNode(NULL, EXPRESSION, ci + 1, (ptr->start + ptr->size) - (ci + 1));
-                }
+
             } else {
-                if(ptr->right && ptr->right->right){
-                    if(nos == 3){
-                        ptr -> right -> left -> format = strdup(word);
-                        ptr -> right -> left -> size = (ptr->start + ptr->size) - ptr -> right -> left -> start;
-                    }else if(nos == 2){
-                        ptr->right->left->format = strdup(word);
-                        ptr->right->left->size = (ptr->start + ptr->size) - ptr->right->left->start;
-                    }
-                }
-                
+                assign_last_word(ptr, word, nos);
             }
+
         } else if (!compare_the_word(word, get_token(ci))) {
             return 1;
         }
@@ -75,9 +105,8 @@ int check_line(struct Node *ptr, char *syn_line){
         ci++;
     }
 
-    return 0; 
+    return 0;
 }
-
 
 int analyze_expression(struct Node *ptr){
     // Go through each line from syntax.txt
